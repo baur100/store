a3a31acf8105a9ffa7729d2fc3a1a8f3540c861f
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-03-21 19:29:54 -0400

    init

--- src/config/database.js
+++ src/config/database.js
@@ -1,38 +1,38 @@
-// import pg from 'pg';
-// import dotenv  from 'dotenv';
-//
-// const { Client } = pg;
-// dotenv.config();
-// const client = new Client({
-//     connectionString: process.env.DATABASE_URL,
-//     ssl: {
-//         rejectUnauthorized: false
-//     }
-// });
-//
-// client.connect();
-//
-// export default {
-//     query: (text, params) => client.query(text, params)
-// };
-//
 import pg from 'pg';
 import dotenv  from 'dotenv';
 
-
-const {Pool} = pg;
-
+const { Client } = pg;
 dotenv.config();
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
+const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
-pool.on('connect', () => {
-    console.log('Connected to database');
-});
-
+client.connect();
 
 export default {
-    query: (text, params) => pool.query(text, params)
+    query: (text, params) => client.query(text, params)
 };
+
+// import pg from 'pg';
+// import dotenv  from 'dotenv';
+//
+//
+// const {Pool} = pg;
+//
+// dotenv.config();
+//
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL
+// });
+//
+// pool.on('connect', () => {
+//     console.log('Connected to database');
+// });
+//
+//
+// export default {
+//     query: (text, params) => pool.query(text, params)
+// };
