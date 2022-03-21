ea798ca321cabc9e970bb0bfba3fc06542f93aec
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-03-21 18:39:14 -0400

    init

--- src/config/database.js
+++ src/config/database.js
@@ -1,45 +1,39 @@
-import pg from 'pg';
-import dotenv  from 'dotenv';
-
-const { Client } = pg;
-dotenv.config();
-const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-
-client.connect();
-
-export default {
-    query: (text, params) => client.query(text, params)
-};
-
-// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-//     if (err) throw err;
-//     for (let row of res.rows) {
-//         console.log(JSON.stringify(row));
-//     }
-//     client.end();
-// });
 // import pg from 'pg';
 // import dotenv  from 'dotenv';
 //
-//
-// const {Pool} = pg;
-//
+// const { Client } = pg;
 // dotenv.config();
-//
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL
+// const client = new Client({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
 // });
 //
-// pool.on('connect', () => {
-//     console.log('Connected to database');
-// });
-//
+// client.connect();
 //
 // export default {
-//     query: (text, params) => pool.query(text, params)
+//     query: (text, params) => client.query(text, params)
 // };
+//
+
+import pg from 'pg';
+import dotenv  from 'dotenv';
+
+
+const {Pool} = pg;
+
+dotenv.config();
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+    console.log('Connected to database');
+});
+
+
+export default {
+    query: (text, params) => pool.query(text, params)
+};
