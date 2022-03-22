ddca1b320db2f9d8b9f122add4e9410b2d821c72
Baurzhan Dosym <baurzhan.zh@gmail.com>
2022-03-21 20:03:29 -0400

    init

--- src/config/database.js
+++ src/config/database.js
@@ -15,24 +15,3 @@ client.connect();
 export default {
     query: (text, params) => client.query(text, params)
 };
-
-// import pg from 'pg';
-// import dotenv  from 'dotenv';
-//
-//
-// const {Pool} = pg;
-//
-// dotenv.config();
-//
-// const pool = new Pool({
-//     connectionString: process.env.DATABASE_URL
-// });
-//
-// pool.on('connect', () => {
-//     console.log('Connected to database');
-// });
-//
-//
-// export default {
-//     query: (text, params) => pool.query(text, params)
-// };
